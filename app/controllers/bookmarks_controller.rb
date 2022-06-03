0b5ddf310950a978e63e97de25f879ec39ac78bf
thomascarre <carrethomas7@gmail.com>
2022-06-03 15:38:04 +0200

    bundle lock --add-platform x86_64-linux

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to '/bookmark', :notice => "Your patient has been deleted"
+  end
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+end
